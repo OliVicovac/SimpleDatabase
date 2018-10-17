5448f814d42a61f5c3fdf508a198ebfa2a90012a
ovicovac <ovicovac@student.tgm.ac.at>
2018-10-17 09:25:44 +0200

    added sqlite

--- src/main/python/server/server.py
+++ src/main/python/server/server.py
@@ -1,11 +1,30 @@
 from flask import Flask;
 from flask_restful import Resource, Api, reqparse
+import sqlite3
 
 app = Flask(__name__)
 api = Api(app)
 
+class Database:
+    def __init__(self):
+        self.conn = sqlite3.connect('database.db')
+
+    def execute(self, query):
+        returnVal = self.conn.execute(query)
+        self.conn.commit()
+        return returnVal
+
+    def close(self):
+        self.conn.close()
+
 class Student(Resource):
+
+    def __init__(self):
+        self.db = Database()
+
     def post(self):
+        self.db.execute('''CREATE TABLE student
+                        (email text, name text, picture text)''')
         return "Formular"
 
 api.add_resource(Student, "/students")
